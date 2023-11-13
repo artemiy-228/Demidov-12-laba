978936f12919407eb71ba3df5c0086021b58c2d7
VorobyovVNS <cmrd.vorobyev@yandex.ru>
2023-11-14 00:34:12 +0500

    ADD MAN COMMIT

---
+++ man.cpp
@@ -0,0 +1,24 @@
+#include "graphics.h"
+
+void man(){
+   int bodyc = COLOR(22, 127, 201);
+   int legsc = COLOR(30, 35, 38);
+   int headc = COLOR(68, 59, 49);
+   
+   setfillstyle(SOLID_FILL, bodyc);
+   setcolor(bodyc);
+   fillellipse(175, 315, 30, 25);
+   fillellipse(225, 315, 30, 25);
+   fillellipse(200, 375, 50, 90);
+   
+   
+   setcolor(BLACK);
+   setfillstyle(SOLID_FILL, legsc);
+   fillellipse(185, 475, 15, 45);
+   fillellipse(215, 475, 15, 45);
+   
+   
+   setfillstyle(SOLID_FILL, headc);
+   fillellipse(200, 275, 35, 25);
+   
+ } // 200, 350
